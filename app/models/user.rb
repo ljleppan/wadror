ce8f9c940ab15d6a849a8221d74e1147caf9e314
ljleppan <leo.leppanen@helsinki.fi>
2014-01-29 18:59:51 +0200

    Fixed password restrictions

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :username
   validates_length_of :username, {minimum: 3, maximum: 15}
   validates_length_of :password, minimum: 4
-  validates_format_of :password, {with: /[A-Z]/,
-                                  with: /[0-9]/}
+  validates_format_of :password, with: /[A-Z]/
+  validates_format_of :password, with: /[0-9]/
 
   has_secure_password
 
